588a45a5f5753ec43c77ebe87c07ad8a909b5b36
Luélly Cabral <luellycabral21@gmail.com>
2020-12-09 20:10:03 -0300

    Adiciona a resposta do exercicio

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Humanizer;
+
+namespace Numeros_Extensos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Numero;
+            string Palavra;
+
+            Console.Write("Digite um Numero: ");
+            Numero = int.Parse(Console.ReadLine());
+            Console.Write("\n");
+
+            Palavra = Numero.ToWords();
+            Console.WriteLine("Seu numero em palavras");
+            Console.WriteLine(Palavra);
+        }
+    }
+}
